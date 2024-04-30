28a54446b0baa873b6b42c4200764c16249ba771
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-04-30 14:35:15 +0800

    Update asynchronous.py

--- asynchronous.py
+++ asynchronous.py
@@ -44,7 +44,7 @@ async def lilypond_generation(melody, name, uppertime, lowertime):
         width, height = img.size
         crop_height = height
         upper = 0
-        lower = 300
+        lower = 250
         crop_rectangle = (0, upper, width, lower)
         cropped_img = img.crop(crop_rectangle)
 
